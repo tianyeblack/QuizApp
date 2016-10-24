27fffee16c574babb5eb97a65dacbfa0107af98f
tianyeblack <tianyeblack@hotmail.com>
2016-10-24 14:45:12 -0700

    Quiz App complete

--- QuizApp/ViewController.m
+++ QuizApp/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (nonatomic) NSArray *questions;
+@property (nonatomic) NSArray *answers;
+@property (weak, nonatomic) IBOutlet UILabel * questionLabel;
+@property (weak, nonatomic) IBOutlet UILabel * answerLabel;
+@property (nonatomic) int currentIndex;
+
 @end
 
 @implementation ViewController
@@ -17,6 +23,16 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.questions = @[@"What is 2 + 2?",
+                       @"What is 1 + 1?",
+                       @"What is the capital of Georgia?"];
+    
+    self.answers = @[@"4",
+                     @"2",
+                     @"Atlanta"];
+    
+    self.questionLabel.text = self.questions[self.currentIndex];
 }
 
 
@@ -25,5 +41,19 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)showNextQuestion: (UIButton *)sender {
+  self.currentIndex++;
+  
+  if (self.currentIndex >= self.questions.count) {
+    self.currentIndex = 0;
+  }
+  
+  self.questionLabel.text = self.questions[self.currentIndex];
+  self.answerLabel.text = @"???";
+}
+
+- (IBAction)showAnswer:(UIButton *)sender {
+  self.answerLabel.text = self.answers[self.currentIndex];
+}
 
 @end
